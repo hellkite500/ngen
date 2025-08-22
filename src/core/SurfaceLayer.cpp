9593813f2d3cd1d111f23c6a5ca9946a68d14802
Ian Todd <itodd@dewberry.com>
2025-08-22 15:15:32 -0400

    Rename model results containers

--- src/core/SurfaceLayer.cpp
+++ src/core/SurfaceLayer.cpp
@@ -4,15 +4,15 @@
  * @brief Run one simulation timestep for each model in this layer, then gather catchment output
 */
 
-void ngen::SurfaceLayer::update_models(boost::span<double> catchment_results,
+void ngen::SurfaceLayer::update_models(boost::span<double> catchment_outflows, 
                                        std::unordered_map<std::string, int> &catchment_indexes,
-                                       boost::span<double> nexus_results,
+                                       boost::span<double> nexus_downstream_flows,
                                        std::unordered_map<std::string, int> &nexus_indexes,
                                        int current_step)
 {
     long current_time_index = output_time_index;
     
-    Layer::update_models(catchment_results, catchment_indexes, nexus_results, nexus_indexes, current_step);
+    Layer::update_models(catchment_outflows, catchment_indexes, nexus_downstream_flows, nexus_indexes, current_step);
 
     //At this point, could make an internal routing pass, extracting flows from nexuses and routing
     //across the flowpath to the next nexus.
@@ -43,7 +43,7 @@ void ngen::SurfaceLayer::update_models(boost::span<double> catchment_results,
 
 #if NGEN_WITH_ROUTING
         int nexus_index = nexus_indexes[id];
-        nexus_results[nexus_index] = contribution_at_t;
+        nexus_downstream_flows[nexus_index] = contribution_at_t;
 #endif // NGEN_WITH_ROUTING
 
         if(nexus_outfiles[id].is_open()) {
